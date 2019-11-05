358e5f39cdc7e477c59014ad45ea6d14e4404415
walkerkehoe <walkerkehoe@gmail.com>
2019-11-05 15:42:35 -0800

    Simple way comms between board and computer

--- board-side.py
+++ board-side.py
@@ -2,25 +2,108 @@
 #screen /dev/tty.usbmodem1411 115200
 
 
+#screen /dev/tty.usbmodem1411 115200
+
 import board
 import digitalio
 import time
 import supervisor
-import busio
 
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
+#led.value = True
+
+class State(object):
+    def __init__(self):
+        pass
+
+    @property
+    def name(self):
+        return ''
+
+    def enter(self, machine):
+        pass
+
+    def exit(self, machine):
+        pass
+
+    def update(self, machine):
+        return True
+
+class StateMachine():
+    def __init__(self):
+        self.state = None
+        self.states = {}
+
+    def add_state(self, state):
+        self.states[state.name] = state
+
+    def go_to_state(self, state_name):
+        if self.state:
+            self.state.exit(self)
+        self.state = self.states[state_name]
+        self.state.enter(self)
+
+    def update(self):
+        if self.state:
+            self.state.update(self)
+
+class ReadyState(object):
+
+    @property
+    def name(self):
+        return 'ready'
+
+    def enter(self, machine):
+        State.enter(self, machine)
+        led.value = False
+
+    def exit(self, machine):
+        State.exit(self, machine)
+
+    def update(self, machine):
+        print('ready\r\n') #read this on comp side using .strip().decode('ascii')
+        if supervisor.runtime.serial_bytes_available:
+            inText = input().strip()
+            if inText == '1':
+                machine.go_to_state('payload')
+        time.sleep(.1)
+
+class PayloadState(object):
+
+    @property
+    def name(self):
+        return 'payload'
+
+    def enter(self, machine):
+        State.enter(self, machine)
+        led.value = True
+
+    def exit(self, machine):
+        State.exit(self, machine)
+        #led.value = True
+
+    def update(self, machine):
+        time.sleep(.1)
+
+machine = StateMachine()
+machine.add_state(ReadyState())
+machine.add_state(PayloadState())
+
+machine.go_to_state('ready')
+
 while True:
-    if supervisor.runtime.serial_bytes_available:
-        inText = input().strip()
-        print("hello")
-        # Sometimes Windows sends an extra (or missing) newline - ignore them
-        if inText == "":
-            continue
-        else:
-            print(inText)
-            led.value = True
-            time.sleep(5)
-            led.value = False
-        #print("received")
+    machine.update()
+
+    #led.value = machine.led_value
+    # if machine.state == 'ready':
+    #     print('ready\r\n') #read this on comp side using .strip().decode('ascii')
+    # if supervisor.runtime.serial_bytes_available:
+    #     inText = input().strip()
+    #     #if inText != '1':
+    #     machine.state = 'comms'
+    #     led.value = True
+    #         #machine.led_value = False
+    #
+    # time.sleep(.1)
--- computer-side.py
+++ computer-side.py
@@ -4,23 +4,30 @@ import time
 ser = serial.Serial()
 ser.baudrate = 115200
 ser.port = '/dev/tty.usbmodem1411'
-ser.timeout = 0.01
+ser.timeout = .01 #why this specific timeout value?
 print(ser)
 ser.open()
 print(ser.is_open)
-ser.write(b'100\r\n')
-x = ser.read(5)
-# x = ser.readlines()
-print("received: {}".format(x))
-# out = ''
-# time.sleep(1)
-# while ser.inWaiting() > 0:
-#     print("here 1")
-#     out += ser.readline().decode().strip()
-#     #out += o.decode('utf-8')
-#     print(out)
-# if out != '':
-#     print(">>" + out)
-time.sleep(1)
+
+#ser.write(b'100\r\n')
+while True:
+#for i in range(1,100):
+    bytes_to_read = ser.inWaiting()
+    print('number of bytes to read: %s' %bytes_to_read)
+    #ser.write('1'.encode('ascii'))
+    x = ser.read(bytes_to_read).strip().decode('ascii')
+    if x:
+        print("received: {}".format(x))
+        to_send = '1\r\n'
+        print('writing to serial: %s' %to_send)
+        ser.write(to_send.encode('ascii'))
+        break
+
+#x = ser.readline().decode('ascii')
+
+
+#time.sleep(1)
+#ser.write('1'.encode('ascii'))
+#time.sleep(1)
 ser.close()
 print(ser.is_open)
