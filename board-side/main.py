6b550a4f30c6b02f749b60ad4021788d4ea209d8
Kevin Crispie <kevincrispie@gmail.com>
2020-02-10 21:11:19 -0800

    5 sensors working!

--- board-side/main.py
+++ board-side/main.py
@@ -7,38 +7,55 @@ import sensor_reads as sensor
 i2c = busio.I2C(board.SCL1, board.SDA1)
 i1c = busio.I2C(board.SCL2, board.SDA2)
 # Create the TSL2561 instance, passing in the I2C bus
-tsl1 = adafruit_tsl2561.TSL2561(i2c,0x39)
-tsl2 = adafruit_tsl2561.TSL2561(i2c,0x49)
-tsl3 = adafruit_tsl2561.TSL2561(i1c,0x29)
+zplus = adafruit_tsl2561.TSL2561(i2c,0x29)
+yplus = adafruit_tsl2561.TSL2561(i2c,0x39)
+xminus = adafruit_tsl2561.TSL2561(i2c,0x49)
+zminus = adafruit_tsl2561.TSL2561(i1c,0x29)
+xplus = adafruit_tsl2561.TSL2561(i1c,0x49)
+
 # Print chip info
 #print("Chip ID = {}".format(tsl1.chip_id))
 #print("Enabled = {}".format(tsl1.enabled))
-#print("Gain = {}".format(tsl1.gain))
-#print("Integration time = {}".format(tsl1.integration_time))
+#print("Gain = {}".format(zplus.gain))
+#print("Integration time = {}".format(zplus.integration_time))
 print("Configuring TSL2561...")
 while True:
     # Enable the light sensor
-    tsl1.enabled = True
-    tsl2.enabled = True
-    tsl3.enabled = True
+    zplus.enabled = True
+    yplus.enabled = True
+    xminus.enabled = True
+    zminus.enabled = True
+    xplus.enabled = True
     time.sleep(1)
     # Set gain 0=1x, 1=16x
-    tsl1.gain = 0
-    tsl2.gain = 0
-    tsl3.gain = 0
+    zplus.gain = 0
+    yplus.gain = 0
+    xminus.gain = 0
+    zminus.gain = 0
+    xplus.gain = 0
     # Set integration time (0=13.7ms, 1=101ms, 2=402ms, or 3=manual)
-    tsl1.integration_time = 1
-    tsl2.integration_time = 1
-    tsl3.integration_time = 1
+    zplus.integration_time = 1
+    yplus.integration_time = 1
+    xminus.integration_time = 1
+    zminus.integration_time = 1
+    xplus.integration_time = 1
     print("Getting readings...")
     # Get raw (luminosity) readings individually
-    broadband1 = tsl1.broadband
-    broadband2 = tsl2.broadband
-    broadband3 = tsl3.broadband
-    
+    zplus_broad = zplus.broadband
+    yplus_broad = yplus.broadband
+    xminus_broad = xminus.broadband
+    zminus_broad = zminus.broadband
+    xplus_broad = xplus.broadband
+
+    print(zplus_broad)
+    print(yplus_broad)
+    print(xminus_broad)
+    print(zminus_broad)
+    print(xplus_broad)
+
     r_sat = [1000,1000,30000]
     q_eci2body = [.8,.2,.4,.3]
-    sunvec = sensor.sense2vector([broadband1, 0,broadband2,0,broadband3,0], r_sat, q_eci2body, albedo = True)
+    sunvec = sensor.sense2vector([xplus_broad, xminus_broad,yplus_broad,0,zplus_broad,zminus_broad], r_sat, q_eci2body, albedo = True)
     print(sunvec)
 
     time.sleep(1)
